4c518f0a206e16037a1dce35e1299d558c3d7117
HotsixMan <hotsixman05@gmail.com>
2026-05-26 11:08:08 +0900

    fix: updating

--- script/module/updateRating.ts
+++ script/module/updateRating.ts
@@ -3,112 +3,150 @@ import { RatingData } from "./types";
 import glicko2 from 'glicko2-lite';
 
 export function updateRating(rankings: { entryTaikoNo: string, totalScore: number }[], ratings: RatingData[], accounts: Account[], days: number) {
-    const map = new RatingDataMap(ratings.map(e => [e.taikoNo, e]));
+    const oldRatingMap = new RatingDataMap(ratings.map(e => [e.taikoNo, e]));
 
     // 정렬 후 대회 계정 제거
     rankings = rankings
         .toSorted((a, b) => {
-            const aRating = map.get(a.entryTaikoNo);
-            const bRating = map.get(b.entryTaikoNo);
+            const aRating = oldRatingMap.get(a.entryTaikoNo);
+            const bRating = oldRatingMap.get(b.entryTaikoNo);
 
             return bRating.rating - aRating.rating;
         })
         .filter((e) => !accounts.map((v) => v.taikoNo).includes(e.entryTaikoNo))
         .filter((e) => e.totalScore !== 0);
 
-    const groupedRankings: { entryTaikoNo: string, totalScore: number }[][] = groupRanking(rankings);
-    const updatedRatingMap = update(groupedRankings, map);
+    const newRatingMap = update(rankings, oldRatingMap);
 
-    scopeAndUpdateRD(ratings, updatedRatingMap, rankings.length)
+    //scopeNewRating(oldRatingMap, newRatingMap, newRatingMap.size);
+    decreaseUnparticipatedRD(ratings, newRatingMap);
 
-    const updatedRating = sortNewRating(updatedRatingMap);
+    const updatedRating = sortNewRating(newRatingMap);
 
     return updatedRating;
 }
 
-function groupRanking(
-    rankings: { entryTaikoNo: string, totalScore: number }[]
-): { entryTaikoNo: string, totalScore: number }[][] {
-    const groups: { entryTaikoNo: string, totalScore: number }[][] = [];
+function update(rankings: { entryTaikoNo: string, totalScore: number }[], map: RatingDataMap) {
+    const updatedRatingMap = new Map<string, RatingData>();
 
-    while (1) {
-        if (rankings.length >= 1000) {
-            groups.push(rankings.slice(0, 1000));
-            rankings = rankings.slice(1000);
-        } else {
-            groups.push(rankings);
-            break;
-        }
+    for (let i = 0; i < rankings.length; i++) {
+        const matches = getMatches(i);
+        const myRating = map.get(rankings[i].entryTaikoNo);
+        const updated = glicko2(myRating.rating, myRating.RD, myRating.Vol, matches);
+        updatedRatingMap.set(rankings[i].entryTaikoNo, {
+            taikoNo: rankings[i].entryTaikoNo,
+            rating: updated.rating,
+            RD: updated.rd,
+            Vol: updated.vol,
+            ranking: 0
+        });
     }
 
-    return groups;
-}
+    function getGlickoScore(myTotalScore: number, opponentRatingScore: number) {
+        /*
+        if (myTotalScore > opponentRatingScore) {
+            return 1;
+        } else if (myTotalScore === opponentRatingScore) {
+            return 0.5;
+        } else {
+            return 0;
+        }
+        */
+        return (myTotalScore) / (myTotalScore + opponentRatingScore);
+    }
 
-function update(groupedRankings: { entryTaikoNo: string, totalScore: number }[][], map: RatingDataMap) {
-    const updatedRatingMap = new Map<string, RatingData>();
-    groupedRankings.forEach((group) => {
-        for (let i = 0; i < group.length; i++) {
+    function getMatches(i: number) {
+        if (rankings.length <= 21) {
             const matches: [number, number, number][] = [];
-            for (let j = 0; j < group.length; j++) {
-                if (i === j) continue;
-                const opponentRating = map.get(group[j].entryTaikoNo);
+            for (let j = 0; j < rankings.length && j !== i; j++) {
+                const opponentRating = map.get(rankings[j].entryTaikoNo);
                 matches.push([
                     opponentRating.rating,
                     opponentRating.RD,
-                    group[i].totalScore > group[j].totalScore ? (1) :
-                        group[i].totalScore === group[j].totalScore ? (0.5) :
-                            0
+                    getGlickoScore(rankings[i].totalScore, rankings[j].totalScore)
                 ]);
             }
-            const myRating = map.get(group[i].entryTaikoNo)
-            const updated = glicko2(myRating.rating, myRating.RD, myRating.Vol, matches);
-
-            updatedRatingMap.set(group[i].entryTaikoNo, {
-                taikoNo: group[i].entryTaikoNo,
-                rating: updated.rating,
-                RD: updated.rd,
-                Vol: updated.vol,
-                ranking: 0
-            });
+            return matches;
         }
-    });
+
+        const upperMatches: [number, number, number][] = [], lowerMatches: [number, number, number][] = [];
+        if (i < rankings.length - 10) {
+            const upperLimit = Math.max(i - 10, 0);
+            for (let j = upperLimit; j < i; j++) {
+                const opponentRating = map.get(rankings[j].entryTaikoNo);
+                upperMatches.push([
+                    opponentRating.rating,
+                    opponentRating.RD,
+                    getGlickoScore(rankings[i].totalScore, rankings[j].totalScore)
+                ]);
+            }
+
+            const lowerLimit = Math.min(i + 20 - upperMatches.length, rankings.length - 1);
+            for (let j = i + 1; j <= lowerLimit; j++) {
+                const opponentRating = map.get(rankings[j].entryTaikoNo);
+                lowerMatches.push([
+                    opponentRating.rating,
+                    opponentRating.RD,
+                    getGlickoScore(rankings[i].totalScore, rankings[j].totalScore)
+                ]);
+            };
+        }
+        else {
+            const lowerLimit = Math.min(i + 10, rankings.length - 1);
+            for (let j = i + 1; j <= lowerLimit; j++) {
+                const opponentRating = map.get(rankings[j].entryTaikoNo);
+                lowerMatches.push([
+                    opponentRating.rating,
+                    opponentRating.RD,
+                    getGlickoScore(rankings[i].totalScore, rankings[j].totalScore)
+                ]);
+            };
+
+            const upperLimit = Math.max(i - 20 + lowerMatches.length, 0);
+            for (let j = upperLimit; j < i; j++) {
+                const opponentRating = map.get(rankings[j].entryTaikoNo);
+                upperMatches.push([
+                    opponentRating.rating,
+                    opponentRating.RD,
+                    getGlickoScore(rankings[i].totalScore, rankings[j].totalScore)
+                ]);
+            }
+        }
+
+        return [...upperMatches, ...lowerMatches];
+    }
+
     return updatedRatingMap;
 }
 
-function scopeAndUpdateRD(ratings: RatingData[], updatedRatingMap: Map<string, RatingData>, participantCount: number) {
-    ratings.forEach((r) => {
-        const newRating = updatedRatingMap.get(r.taikoNo);
-        if (newRating) {
-            // 변동폭 조정
-            updatedRatingMap.set(newRating.taikoNo, {
-                taikoNo: newRating.taikoNo,
-                rating: r.rating + (newRating.rating - r.rating) / participantCount,
-                RD: newRating.RD,
-                Vol: newRating.Vol,
-                ranking: 0
-            })
-        }
-        else {
-            // RD 감소 및 레이팅 감소
-            const RD = Math.min(Math.sqrt(r.RD ** 2 + 1 * (r.Vol ** 2)), 350);
-            updatedRatingMap.set(r.taikoNo, {
-                taikoNo: r.taikoNo,
-                rating: r.ranking - 10,
-                RD,
-                Vol: r.Vol,
-                ranking: 0
-            })
-        }
+function scopeNewRating(oldRatings: RatingDataMap, newRatings: Map<string, RatingData>, participantCount: number) {
+    newRatings.forEach((newRatingData, taikoNo) => {
+        const oldRatingData = oldRatings.get(taikoNo);
+        newRatingData.rating = (oldRatingData.rating) + (newRatingData.rating - oldRatingData.rating) / Math.sqrt(participantCount);
     });
 }
 
-function sortNewRating(updatedRatingMap: Map<string, RatingData>) {
-    const updatedRating = Array.from(updatedRatingMap.values());
-    updatedRating.sort((a, b) => b.rating - a.rating)
-    updatedRating.forEach((e, i) => {
+function decreaseUnparticipatedRD(oldRatings: RatingData[], newRatingMap: Map<string, RatingData>) {
+    oldRatings.forEach((r) => {
+        if (newRatingMap.has(r.taikoNo)) return;
+        const RD = Math.min(Math.sqrt(r.RD ** 2 + 1 * (r.Vol ** 2)), 350);
+        newRatingMap.set(r.taikoNo, {
+            taikoNo: r.taikoNo,
+            rating: r.ranking - 10,
+            RD,
+            Vol: r.Vol,
+            ranking: 0
+        });
+    })
+}
+
+function sortNewRating(newRatingMap: Map<string, RatingData>) {
+    const newRatingDatas = Array.from(newRatingMap.values());
+    newRatingDatas.sort((a, b) => b.rating - a.rating)
+    newRatingDatas.forEach((e, i) => {
         e.ranking = i + 1;
     });
-    return updatedRating;
+    return newRatingDatas;
 }
 
 class RatingDataMap {
--- script/tests/update-rating/2/index.ts
+++ script/tests/update-rating/2/index.ts
@@ -1,17 +1,21 @@
 import path from "node:path";
 import { updateRating } from "../../../module/updateRating";
+import { RatingData } from "../../../module/types";
 
 // 100명이서 시작시 변동 측정
 const rankings: {
     entryTaikoNo: string;
     totalScore: number;
 }[] = [];
+const ratings: RatingData[] = [];
 
 for (let i = 0; i < 100; i++) {
     rankings.push({
         entryTaikoNo: `${i}`,
         totalScore: 1000 - i
-    })
+    });
+   
 };
 
-await Bun.write(path.join(import.meta.dir, 'output.json'), JSON.stringify(updateRating(rankings, [], [], 1), null, 2))
+
+await Bun.write(path.join(import.meta.dir, 'output.json'), JSON.stringify(updateRating(rankings, ratings, [], 1), null, 2))
